01b798fe37e0c09023a9aca456c76c27cb51edd8
Suraj Singh <surajstranger11@gmail.com>
2018-12-18 22:07:30 +0530

    Single recipe is being displayed

--- CourseProject/src/app/recipes/recipe-list/recipe-list.component.ts
+++ CourseProject/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipes.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+ recipes: Recipe[]= [
+   new Recipe('A test Recipe','This is simply a test',
+   'https://images.pexels.com/photos/247685/pexels-photo-247685.png?cs=srgb&dl=assorted-diet-edible-247685.jpg&fm=jpg')
+ ];
   constructor() { }
 
   ngOnInit() {
